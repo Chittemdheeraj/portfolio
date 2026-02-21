cd6a019c35fda19539835159521466be27c3558c
Chittem Dheeraj <chittemdheeraj22@gmail.com>
2026-02-21 15:23:47 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// Simple smooth scrolling for navigation links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
